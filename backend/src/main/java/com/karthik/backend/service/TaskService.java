06fa68a7f5faa5aebb3df5bbd06cfd98a6670f77
Karthikmelam28 <karthikmelam4@gmail.com>
2026-06-27 22:24:44 +0530

    feat: complete Sprint 3 CRUD operations

--- backend/src/main/java/com/karthik/backend/service/TaskService.java
+++ backend/src/main/java/com/karthik/backend/service/TaskService.java
@@ -1,8 +1,10 @@
 package com.karthik.backend.service;
 
+import java.util.List;
 import com.karthik.backend.entity.Task;
 import com.karthik.backend.repository.TaskRepository;
 import org.springframework.stereotype.Service;
+import com.karthik.backend.exception.TaskNotFoundException;
 
 import java.util.List;
 
@@ -21,5 +23,30 @@ public class TaskService {
 
     public Task saveTask(Task task) {
         return taskRepository.save(task);
+    }
+
+    public Task getTaskById(Long id) {
+        return taskRepository.findById(id)
+                .orElseThrow(() -> new TaskNotFoundException(id));
+    }
+
+    public Task updateTask(Long id, Task updatedTask) {
+
+        Task existingTask = taskRepository.findById(id)
+                .orElseThrow(() -> new TaskNotFoundException(id));
+
+        existingTask.setTitle(updatedTask.getTitle());
+        existingTask.setDescription(updatedTask.getDescription());
+        existingTask.setStatus(updatedTask.getStatus());
+
+        return taskRepository.save(existingTask);
+    }
+
+    public void deleteTask(Long id) {
+
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new TaskNotFoundException(id));
+
+        taskRepository.delete(task);
     }
 }
